07627d8fe14c2b98261ea0e96fc23768cd61a0a1
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2024-02-18 20:57:56 +0300

    commit with all changes_3

--- bot/src/main/java/edu/java/bot/util/MessagesUtils.java
+++ bot/src/main/java/edu/java/bot/util/MessagesUtils.java
@@ -9,13 +9,13 @@ import static edu.java.bot.command.Command.UNTRACK;
 @UtilityClass
 public class MessagesUtils {
 
-    public static final String LINK_HAS_BEEN_UNTRACKED = "Ссылка больше не отслеживается.";
-    public static final String CHOOSE_LINK_TO_UNTRACK = "Выберите ссылку, у которой нужно прекратить отслеживание: ";
+    public static final String LINK_HAS_BEEN_UNTRACKED = "Выбранная ссылка больше не отслеживается.";
+    public static final String CHOOSE_LINK_TO_UNTRACK = "Выберите ссылку, отслеживание которой необходимо прекратить: ";
     public static final String NO_TRACKED_LINKS =
-        "Отслеживаемых ссылок нет 😥. Добавьте ссылку с помощью команды /track.";
+        "Отслеживаемых ссылок нет ❎. Добавьте ссылку с помощью команды /track.";
     public static final String WELCOME_MESSAGE = """
-        <b>Привет! 😊</b>
-        Это бот для отслеживания изменений на GitHub и Stack Overflow. 🚀
+        <b>Привет! 🖐️</b>
+        Это бот для отслеживания изменений на GitHub и Stack Overflow.
         Справка по командам - /help""";
     public static final String TRACKED_LINKS = "Отслеживаемые ссылки: ";
     public static final String HELP_MESSAGE = """
@@ -30,7 +30,7 @@ public class MessagesUtils {
         HELP.getName(), HELP.getDescription().toLowerCase()
     );
     public static final String ERROR_MESSAGE = """
-        <b>Ошибка:</b> Команда не существует. ❌
+        <b>Ошибка:</b> Данной команды не существует. ❌
         Пожалуйста, воспользуйтесь командой /help для получения списка доступных команд.
         """;
     public static final String ONLY_TEXT_TO_SEND = "Для отправки доступен только текст!";
