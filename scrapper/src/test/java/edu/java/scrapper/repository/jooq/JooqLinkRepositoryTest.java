d8ea4979b00a6949af4ec5e8a7abc73ec59e69ff
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2024-04-09 19:19:13 +0300

    with all changes in tests_2

--- scrapper/src/test/java/edu/java/scrapper/repository/jooq/JooqLinkRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/repository/jooq/JooqLinkRepositoryTest.java
@@ -257,22 +257,4 @@ public class JooqLinkRepositoryTest extends IntegrationEnvironment {
         assertThat(uncheckedLinks).isEmpty();
     }
 
-    @Test
-    @Transactional
-    @Rollback
-    public void updateInfo_shouldUpdateLastCheckedAtAndUpdatedAt() {
-        // Arrange
-        String url = "google.com";
-        OffsetDateTime updatedAt = OffsetDateTime.now();
-
-        long linkId = linkRepository.add(1L, new AddLinkRequest(url), LinkType.UNKNOWN);
-
-        // Act
-        linkRepository.updateInfo(linkId, updatedAt);
-
-        // Assert
-        LinkData linkData = linkRepository.findById(linkId).orElseThrow();
-        assertThat(linkData.lastCheckedAt()).isEqualToIgnoringNanos(OffsetDateTime.now());
-        assertThat(linkData.updatedAt()).isEqualToIgnoringNanos(updatedAt);
-    }
 }
