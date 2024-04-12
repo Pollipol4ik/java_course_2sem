0543da5d9740e951a9e25b26d0f7066553729bc7
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2024-04-12 22:28:29 +0300

    only logic in two modules

--- bot/src/main/java/edu/java/bot/configuration/BotConfiguration.java
+++ bot/src/main/java/edu/java/bot/configuration/BotConfiguration.java
@@ -1,39 +1,11 @@
 package edu.java.bot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.BotCommand;
-import edu.java.bot.client.ScrapperClient;
-import edu.java.bot.command.CommandChain;
-import edu.java.bot.resolver.UpdateCallbackResolver;
-import edu.java.bot.resolver.UpdateMessageResolver;
-import edu.java.bot.resolver.UpdateResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static edu.java.bot.command.Command.HELP;
-import static edu.java.bot.command.Command.LIST;
-import static edu.java.bot.command.Command.TRACK;
-import static edu.java.bot.command.Command.UNTRACK;
 
 @Configuration
 public class BotConfiguration {
-
-    @Bean
-    public UpdateResolver updateResolver(ScrapperClient scrapperClient, CommandChain commandChain) {
-        return UpdateResolver.link(
-            new UpdateMessageResolver(commandChain),
-            new UpdateCallbackResolver(scrapperClient)
-        );
-    }
-
-    @Bean
-    public BotCommand[] commands() {
-        return new BotCommand[] {
-            new BotCommand(TRACK.getName(), TRACK.getDescription()),
-            new BotCommand(UNTRACK.getName(), UNTRACK.getDescription()),
-            new BotCommand(LIST.getName(), LIST.getDescription()),
-            new BotCommand(HELP.getName(), HELP.getDescription())
-        };
-    }
 
     @Bean
     public TelegramBot bot(ApplicationConfig applicationConfig) {
