471980c8aa8725a17248a3e263483a51dd85317d
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2024-05-02 22:30:39 +0300

    changes in ScrapperClient

--- bot/src/main/java/edu/java/bot/client/ScrapperClient.java
+++ bot/src/main/java/edu/java/bot/client/ScrapperClient.java
@@ -4,37 +4,67 @@ import edu.java.bot.dto.AddLinkRequest;
 import edu.java.bot.dto.ListLinksResponse;
 import edu.java.bot.dto.RemoveLinkRequest;
 import edu.java.bot.dto.ResponseLink;
-import edu.java.bot.service.ScrapperService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
-public class ScrapperClient extends AbstractClient<ScrapperService> {
+@Component
+@RequiredArgsConstructor
+public class ScrapperClient {
+    private static final String CHAT_ENDPOINT = "/tg-chat";
+    private static final String LINK_ENDPOINT = "/links";
+    private static final String TG_CHAT_HEADER = "Tg-Chat-Id";
 
-    private static final String SCRAPPER_BASE_URL = "http://localhost:8080/";
+    private final WebClient webClient;
 
-    public ScrapperClient() {
-        this(SCRAPPER_BASE_URL);
+    public ListLinksResponse getLinks(Long chatId) {
+        return webClient
+            .get()
+            .uri(LINK_ENDPOINT)
+            .header(TG_CHAT_HEADER, chatId.toString())
+            .retrieve()
+            .bodyToMono(ListLinksResponse.class)
+            .block();
     }
 
-    public ScrapperClient(String baseUrl) {
-        super(baseUrl);
+    public ResponseLink addLink(Long chatId, AddLinkRequest addLinkRequest) {
+        return webClient
+            .post()
+            .uri(LINK_ENDPOINT)
+            .header(TG_CHAT_HEADER, chatId.toString())
+            .bodyValue(addLinkRequest)
+            .retrieve()
+            .bodyToMono(ResponseLink.class)
+            .block();
     }
 
-    public ListLinksResponse getAllTrackedLinks(long chatId) {
-        return service.getAllLinks(chatId);
+    public ResponseLink removeLink(Long chatId, RemoveLinkRequest removeLinkRequest) {
+        return webClient
+            .method(HttpMethod.DELETE)
+            .uri(LINK_ENDPOINT)
+            .header(TG_CHAT_HEADER, chatId.toString())
+            .bodyValue(removeLinkRequest)
+            .retrieve()
+            .bodyToMono(ResponseLink.class)
+            .block();
     }
 
-    public ResponseLink trackLink(long chatId, AddLinkRequest addLinkRequest) {
-        return service.trackLink(chatId, addLinkRequest);
+    public void registerChat(Long chatId) {
+        webClient
+            .post()
+            .uri(CHAT_ENDPOINT, chatId)
+            .retrieve()
+            .bodyToMono(Void.class)
+            .block();
     }
 
-    public ResponseLink untrackLink(long chatId, RemoveLinkRequest removeLinkRequest) {
-        return service.untrackLink(chatId, removeLinkRequest);
-    }
-
-    public void registerChat(long chatId) {
-        service.registerChat(chatId);
-    }
-
-    public void deleteChat(long chatId) {
-        service.deleteChat(chatId);
+    public void deleteChat(Long chatId) {
+        webClient
+            .delete()
+            .uri(CHAT_ENDPOINT, chatId)
+            .retrieve()
+            .bodyToMono(Void.class)
+            .block();
     }
 }
